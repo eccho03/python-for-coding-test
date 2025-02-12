7d9eb90213e4eb56021af075f3255544a2e94ce3
Eunchae Cho <choeunchae0828@gmail.com>
2025-02-12 13:06:29 +0900

    6번 내 풀이 - 수정

--- ch01. 그리디/6.py
+++ ch01. 그리디/6.py
@@ -18,3 +18,8 @@ def solution(food_times, k):
     return answer
 
 print(solution([3,1,2], 5))
+
+# 어떻게 하면 O(N^2) -> O(NlogN)으로 줄일 수 있을지 고민해보기
+# 조건이 까다로울 수록 단순하게 만들기 위해 노력할 것
+# -1 출력 -> 최대한 빨리 처리될 방법은 없을지 생각
+# NlogN? -> quick sort, heap sort, priority queue, ... 등을 떠올리기
