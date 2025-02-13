3c0dfd4371df165a803dfd5f3598484e2e014fbd
Eunchae Cho <choeunchae0828@gmail.com>
2025-02-13 17:40:19 +0900

    2번 내 풀이

--- ch02. 구현/2.py
+++ ch02. 구현/2.py
@@ -1,8 +1,26 @@
-s = str(input())
+s = input()
 
-string = list(map(str, str(s)))
+string = list(s)
 
-for i in string:
-    if i == 0 or i == 1:
-        i = int(i)
-print(string)
+number = "1234567890"
+
+arr_num = []
+arr_str = []
+
+for i in range(len(string)):
+    if number.find(string[i]) != -1:
+        string[i] = int(string[i])
+        arr_num.append(string[i])
+    else:
+        arr_str.append(string[i])
+
+arr_str.sort()
+
+for i in arr_str:
+    print(i, end='')
+
+sum_int = 0
+
+for i in arr_num:
+    sum_int += i
+print(sum_int)
