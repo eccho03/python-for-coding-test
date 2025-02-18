6072ed76665c0c15e208eda9038865ebe307931f
Eunchae Cho <choeunchae0828@gmail.com>
2025-02-18 14:40:26 +0900

    12번 내 풀이 - 보완점

--- ch02. 구현/12.py
+++ ch02. 구현/12.py
@@ -64,3 +64,9 @@ def solution(n, build_frame):
         answer = [[x, y, 0] for x, y in gi] + [[x, y, 1] for x, y in bo]
         
     return sorted(answer)
+
+# answer 낼 때 + 를 사용해서 각각이 기둥과 보 각각이 아니라 한 번에 고려
+# answer sort하는 거 잊지 말기
+# 기둥이나 보를 삭제한 이후에 구조가 유지되는지 확인 중요 !!
+# for문 인덱스 두 개 사용하여 순회하면서 [x, y] 이런 식으로 순회하는 방법이 있다
+# 체크할 때는 삭제 때문에 해당 기둥이나 보 말고 전체를 확인해야 함
